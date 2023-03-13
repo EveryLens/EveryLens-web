9371b9a38ceebb2ba078b6c120d13c7b5127e403
Ricy <1932353948@qq.com>
2023-03-13 17:41:54 +0800

    feat: create claim

---
+++ app/create/page.tsx
@@ -0,0 +1,9 @@
+const Create: React.FC = () => {
+  return (
+    <div className="flex flex-col">
+      Here're some explainations of how EveryLens works
+      <button>Claim</button>
+    </div>
+  );
+};
+export default Create;
